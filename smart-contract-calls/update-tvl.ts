e1968b2e39439120f2b4fb135a8cfe3797aaa73f
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2025-08-17 16:02:07 +0200

    update tvl at every voting round

---
+++ smart-contract-calls/update-tvl.ts
@@ -0,0 +1,31 @@
+import { createRedisInstance, logError } from "./common";
+
+run();
+
+async function run() {
+
+    const redis = createRedisInstance()
+    const members: any[] = (await redis.get("MEMBERS") as any).all
+
+    const totalStake = members.map((m: any) => m.umaStake).reduce((partialSum, a) => partialSum + a, 0);
+    const currentTimestamp = Math.round(Date.now() / 1000)
+    
+    const newTvl = {
+        "timestamp": currentTimestamp,
+        "stake": totalStake
+    }
+
+    let tvlHistory: any[] = (await redis.get("TVL_HISTORY") as any).all
+    tvlHistory.push(newTvl)
+
+    try {
+            
+        await redis.set("TVL_HISTORY", tvlHistory)
+
+    } catch (error) {
+        
+        await logError(error.message, `Could not update the TVL history`)
+
+    }
+
+}
