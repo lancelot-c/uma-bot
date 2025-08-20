52a13ee62623fc858f49b1a3e21c5d6991233fa6
Lancelot Chardonnet <1041338+lancelot-c@users.noreply.github.com>
2025-08-20 10:49:07 +0200

    fix update-tvl.ts

--- smart-contract-calls/update-tvl.ts
+++ smart-contract-calls/update-tvl.ts
@@ -8,14 +8,20 @@ async function run() {
     const redis = createRedisInstance()
     const members: any[] = (await redis.get("MEMBERS") as any).all
 
-    const totalStake = members.map((m: any) => m.umaStake).reduce((partialSum, a) => partialSum + a, 0);
-    const currentTimestamp = Math.round(Date.now() / 1000)
+    const totalStake = Math.round(members.map((m: any) => m.umaStake).reduce((partialSum, a) => partialSum + a, 0));
+    // const currentTimestamp = Math.round(Date.now() / 1000)
+    const currentDate = (new Date()).toISOString().split('T')[0]
     const currentUmaPrice = await getCurrentUmaPrice()
     
-    const newTvl = {
-        "t": currentTimestamp,
-        "s": totalStake,
-        "p": currentUmaPrice
+    let newTvl = {
+        "t": currentDate,
+        "s": totalStake
+    }
+
+    if (currentUmaPrice) {
+        newTvl["p"] = currentUmaPrice
+    } else {
+        logError('currentUmaPrice is undefined')
     }
 
     let tvlHistory: any[] = (await redis.get("TVL_HISTORY") as any).all
